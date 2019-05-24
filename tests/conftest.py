24bb0b5d2dde1ce055721dfdab286ecdcb2bdfa8
Edwin Steele <edwin@wordspeak.org>
2019-05-24 17:10:17 +1000

    Move to pytest fixtures and fn based tests

---
+++ tests/conftest.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import pathlib
+import pytest
+
+@pytest.fixture
+def iw_dev_scan_0():
+    p = pathlib.Path("tests/fixtures/iw_dev_scan_0.txt")
+    return p.read_text()
+
+@pytest.fixture
+def iw_dev_scan_1():
+    p = pathlib.Path("tests/fixtures/iw_dev_scan_1.txt")
+    return p.read_text()
+
+@pytest.fixture
+def iw_dev_scan_2():
+    p = pathlib.Path("tests/fixtures/iw_dev_scan_2.txt")
+    return p.read_text()
+
+@pytest.fixture
+def iw_dev_scan_3():
+    # This has tabs instead of spaces
+    p = pathlib.Path("tests/fixtures/iw_dev_scan_3.txt")
+    return p.read_text()
--- tests/test_scan.py
+++ tests/test_scan.py
@@ -1,45 +1,25 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import pathlib
-import unittest
 from wifi_configurator import scan
 
+def test_get_country_count_populated(iw_dev_scan_0):
+    c = scan.get_country_count_from_iw_output(iw_dev_scan_0)
+    assert c.most_common(1)[0][0] == "TR"
+    assert len(list(c.elements())) == 4
+    assert c["AL"] == 1
 
-class TestScan(unittest.TestCase):
+def test_get_country_count_unpopulated(iw_dev_scan_1):
+    c = scan.get_country_count_from_iw_output(iw_dev_scan_1)
+    assert c.most_common(1) == []
+    assert not list(c.elements())
 
-    def setUp(self):
-        """Set up test fixtures, if any."""
+def test_get_country_count_empty(iw_dev_scan_2):
+    c = scan.get_country_count_from_iw_output(iw_dev_scan_2)
+    assert c.most_common(1) == []
+    assert not list(c.elements())
 
-    def tearDown(self):
-        """Tear down test fixtures, if any."""
-
-    def test_get_country_count_populated(self):
-        p = pathlib.Path("tests/fixtures/iw_dev_scan_0.txt")
-        test_output = p.read_text()
-        c = scan.get_country_count_from_iw_output(test_output)
-        self.assertEqual(c.most_common(1)[0][0], "TR")
-        self.assertEqual(len(list(c.elements())), 4)
-        self.assertEqual(c["AL"], 1)
-
-    def test_get_country_count_unpopulated(self):
-        p = pathlib.Path("tests/fixtures/iw_dev_scan_1.txt")
-        test_output = p.read_text()
-        c = scan.get_country_count_from_iw_output(test_output)
-        self.assertEqual(c.most_common(1), [])
-        self.assertEqual(len(list(c.elements())), 0)
-
-    def test_get_country_count_empty(self):
-        p = pathlib.Path("tests/fixtures/iw_dev_scan_2.txt")
-        test_output = p.read_text()
-        c = scan.get_country_count_from_iw_output(test_output)
-        self.assertEqual(c.most_common(1), [])
-        self.assertEqual(len(list(c.elements())), 0)
-
-    def test_get_country_count_populated2(self):
-        # This has tabs instead of spaces
-        p = pathlib.Path("tests/fixtures/iw_dev_scan_3.txt")
-        test_output = p.read_text()
-        c = scan.get_country_count_from_iw_output(test_output)
-        self.assertEqual(c.most_common(1)[0][0], "AU")
-        self.assertEqual(len(list(c.elements())), 2)
+def test_get_country_count_populated2(iw_dev_scan_3):
+    c = scan.get_country_count_from_iw_output(iw_dev_scan_3)
+    assert c.most_common(1)[0][0] == "AU"
+    assert len(list(c.elements())) == 2
